73c5710215b4db66c2189a369b92136023a6c7d7
Miguel Angel <78570710+Miguel-HH@users.noreply.github.com>
2021-06-12 13:46:59 -0500

    test: component SectionProjects

--- src/components/organisms/mains/Main.js
+++ src/components/organisms/mains/Main.js
@@ -11,91 +11,91 @@ function Main() {
         <Fragment>
             <section id="Main"></section>
             <SectionTextImage
-                title="Cuando quieras y donde quieras."
-                subtitle="Todas las páginas están diseñadas para que las disfrutes en tu pantalla favorita. Ya sea tu TV, tableta, computadora, o teléfono."
-                logo={Image1}
+                title = 'Cuando quieras y donde quieras'
+                subtitle = 'Todas las páginas están diseñadas para que las disfrutes en tu pantalla favorita. Ya sea tu TV, tableta, computadora, o teléfono.'
+                logo = {Image1}
             />
             <SectionImageText
-                title="Detrás de cámaras y descargas ilimitadas"
-                subtitle="¡Código libre y bien documentado! Todas las páginas están disponibles en GitHub. Puedes descargar o clonar los repositorios cuantas veces quieras."
-                logo={Image2}
+                title = 'Detrás de cámaras y descargas ilimitadas'
+                subtitle = '¡Código libre y bien documentado! Todas las páginas están disponibles en GitHub. Puedes descargar o clonar los repositorios cuantas veces quieras.'
+                logo = {Image2}
             />
             <section>Últimos estrenos</section>
             <SectionProjects
-                title="titulo inicial 1"
-                subtitle="subtitulo inicial 1"
-                image0={Image1}
+                title = 'Blog de bolsillo'
+                subtitle = 'Todos los apuntes que están en tu celular se pueden publicar en "GitHub Pages"'
+                image0 = {Image1}
 
-                title1="titulo alternativo A1"
-                subtitle1="subtitulo alternativo A1"
-                image1={Image2}
-                button1="clic aquí A1"
+                title1 = 'Todo es posible con CSS'
+                subtitle1 = 'Menu desplegable y cambio de tema con CSS. Sin utilizar JavaScript'
+                image1 = {Image2}
+                button1 = 'CSS'
 
-                title2="titulo alternativo B1"
-                subtitle2="subtitulo alternativo B1"
-                image2={Image1}
-                button2="clic aquí B1"
+                title2 = 'SEO y Accesibilidad'
+                subtitle2 = 'Puntaje perfecto en "Lighthouse".'
+                image2 = {Image1}
+                button2 = 'Lighthouse'
 
-                title3="titulo alternativo C1"
-                subtitle3="subtitulo alternativo C1"
-                image3={Image2}
-                button3="clic aquí C1"
+                title3 = 'Todo está escrito en texto plano'
+                subtitle3 = 'Todos los post están escritos en "Markdown".'
+                image3 = {Image2}
+                button3 = 'Markdown'
 
-                title4="titulo alternativo D1"
-                subtitle4="subtitulo alternativo D1"
-                image4={Image1}
-                button4="clic aquí D1"
+                title4 = 'GitHub Pages'
+                subtitle4 = 'Jekyll y GitHub Pages convierten los archivos tipo markdown en HTML.'
+                image4 = {Image1}
+                button4 = 'Ver ahora ▶'
             />
             <SectionProjects
-                title="titulo inicial 02"
-                subtitle="subtitulo inicial 02"
-                image0={Image2}
+                title = 'Portafolio'
+                subtitle = 'Con sabor a página de Streaming.'
+                image0 = {Image2}
 
-                title1="titulo alternativo A2"
-                subtitle1="subtitulo alternativo A2"
-                image1={Image1}
-                button1="clic aquí A2"
+                title1 = 'Página construida con React.js'
+                subtitle1 = 'Todas los componenetes son reutilizables.'
+                image1 = {Image1}
+                button1 = 'React.js'
 
-                title2="titulo alternativo B2"
-                subtitle2="subtitulo alternativo B2"
-                image2={Image2}
-                button2="clic aquí B2"
+                title2 = 'React Hooks'
+                subtitle2 = 'Los cambios de estado facilitan la lectura del código.'
+                image2 = {Image2}
+                button2 = 'Hooks'
 
-                title3="titulo alternativo C2"
-                subtitle3="subtitulo alternativo C2"
-                image3={Image1}
-                button3="clic aquí C2"
+                title3 = 'Diseño atómico, a-tó-mi-co'
+                subtitle3 = 'Las etiquetas HTML forman átomos, que a su vez forman componentes, que a su vez forman plantillas, que a su vez...'
+                image3 = {Image1}
+                button3 = 'Atómico'
 
-                title4="titulo alternativo D2"
-                subtitle4="subtitulo alternativo D2"
-                image4={Image1}
-                button4="clic aquí D2"
+                title4 = 'Imágenes propias'
+                subtitle4 = 'SVG hecho con Illustrator'
+                image4 = {Image1}
+                button4 = 'Arte'
             />
-            <section>Próximos estrenos</section>
+            <section>Un vistazo a los próximos lanzamientos</section>
             <SectionProjects
-                title="titulo inicial 03"
-                subtitle="subtitulo inicial 03"
-                image0={Image1}
+                title = 'Este 2021'
+                subtitle = 'Nuevos proyectos.'
+                image0 = {Image1}
 
-                title1="titulo alternativo A3"
-                subtitle1="subtitulo alternativo A3"
-                image1={Image2}
-                button1="clic aquí A3"
+                title1 = 'API'
+                subtitle1 = 'Ajax y WebSockets'
+                image1 = {Image2}
+                button1 = 'API'
 
-                title2="titulo alternativo B3"
-                subtitle2="subtitulo alternativo B3"
-                image2={Image1}
-                button2="clic aquí B3"
+                title2 = 'JavaScript puro'
+                subtitle2 = 'Una app escrita con JavaScript puro. Programación orientada a objetos.'
+                image2 = {Image1}
+                button2 = 'JavaScript'
 
-                title3="titulo alternativo C3"
-                subtitle3="subtitulo alternativo C3"
-                image3={Image2}
-                button3="clic aquí C3"
+                title3 = 'Portafolio - Segunda Temporada'
+                subtitle3 = 'Mejorar el código'
+                image3 = {Image2}
+                button3 = 'Portafolio 2'
 
-                title4="titulo alternativo D3"
-                subtitle4="subtitulo alternativo D3"
-                image4={Image1}
-                button4="clic aquí D3"
+                title4 = 'Blog - Segunda Temporada'
+                subtitle4 = 'Mejorar el código'
+                image4 = {Image1}
+                button4 = 'Blog 2'
             />
         </Fragment>
     )
